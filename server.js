786e5c8945a874d3760f71b6bebc35a2f2c77d74
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-09 15:55:44 -0600

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ mongoose.connect(MONGODB_URI,{  useNewUrlParser: true, useUnifiedTopology: true,
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 app.use(routes)
 
 app.listen(PORT, () => {
