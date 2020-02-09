6dc5829d192b912cc97fd4029c4fbfc263fe6e45
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-09 16:24:11 -0600

    Update index.js

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,7 +6,7 @@ return (
   <div className="container">
   <form>
     <div className="form-group">
-      <label htmlFor="search"><h2>Search for and save Books of Interest</h2></label>
+      <label htmlFor="search"><h2>Search for books!  Save them for later!</h2></label>
       <input
         onChange={props.handleInputChange}
         search={props.search}
