88f015be252a3d0637fe514c3b9199cda11a52b0
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-08 18:00:29 -0600

    Cleaned up code
    took out useless snippets

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,18 +2,8 @@ import axios from "axios"
 
 export default {
     // Get book from google search 
-    getGoogleSearchBooks: function(query) {
+    getGoogleResults: function(query) {
         return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
-    },
-
-    // Gets all books
-    getBooks: function () {
-        return axios.get("/books");
-    },
-
-    // Gets the book with the given id
-    getBook: function (id) {
-        return axios.get("/books/" + id);
     },
 
     // Saves a book to the database
@@ -24,6 +14,9 @@ export default {
     // Deletes the book with the given id
     deleteBook: function (id) {
         return axios.delete("/books/" + id);
-    }
+    },
 
+    savedBooks: function () {
+        return axios.get("/api/books").then(result => result.data);
+      }
 }
